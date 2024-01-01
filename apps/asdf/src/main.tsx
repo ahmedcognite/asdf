cfa65a56cbdd5fde0b9c25585f729ee2eb9a79fe
Ahmed Noor E Alam <104756122+ahmedcognite@users.noreply.github.com>
2024-01-01 20:53:53 +0100

    refactor: separate local and locize i18n inits (#7)
    * refactor: separate local and locize i18n inits

--- apps/asdf/src/main.tsx
+++ apps/asdf/src/main.tsx
@@ -3,7 +3,10 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app';
 
-import './translations/i18n';
+import './translations/i18nWithLocize';
+// uncomment if you want to use local translation files
+// and comment the above import
+// import './translations/i18nLocal';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
