3a04d3c061c17b2269ee5916930feb1c7893410a
Ahmed Noor E Alam <104756122+ahmedcognite@users.noreply.github.com>
2024-01-01 03:39:51 +0100

    add: i18n and scaffolding for api sdk (#4)

--- apps/asdf/src/app.tsx
+++ apps/asdf/src/app.tsx
@@ -1,12 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useTranslation } from 'react-i18next';
 import styles from './app.module.scss';
 
 import { Button } from '@asdf/ui';
 
 export function App() {
+  const {
+    t,
+    i18n: { changeLanguage, language },
+  } = useTranslation();
   return (
     <div>
-      <Button>Click me!</Button>
+      <Button
+        onClick={() => {
+          console.log(language);
+          language === 'en' ? changeLanguage('nb') : changeLanguage('en');
+        }}
+      >
+        {t('generic_button_text')}
+      </Button>
     </div>
   );
 }
