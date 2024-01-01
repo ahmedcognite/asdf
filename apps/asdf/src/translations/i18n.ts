30482904922033b8cc4dd7e7e95cc2e9da35972b
Ahmed Noor E Alam <104756122+ahmedcognite@users.noreply.github.com>
2024-01-01 20:26:58 +0100

    add: locize integration (#6)
    * add: locize integration

--- apps/asdf/src/translations/i18n.ts
+++ apps/asdf/src/translations/i18n.ts
@@ -1,18 +1,55 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enJSON from './locale/en.json';
-import nbJSON from './locale/nb.json';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import LocizeBackend from 'i18next-locize-backend';
+
+// import enJSON from './locale/en.json';
+// import nbJSON from './locale/nb.json';
+
+const LOCIZE_PROJECT_ID = process.env.LOCIZE_PROJECT_ID;
+const LOCIZE_API_KEY = process.env.LOCIZE_API_KEY;
 
 export const initI18n = () => {
-  i18n.use(initReactI18next).init({
-    resources: {
-      // Where we're gonna put translations' files
-      en: { ...enJSON },
-      nb: { ...nbJSON },
-    },
-    lng: 'en', // Set the initial language of the App
-    fallbackLng: 'en', // Set the initial language of the App
-  });
+  // i18n.use(initReactI18next).init({
+  //   resources: {
+  //     // Where we're gonna put translations' files
+  //     en: { ...enJSON },
+  //     nb: { ...nbJSON },
+  //   },
+  //   lng: 'en', // Set the initial language of the App
+  //   fallbackLng: 'en', // Set the initial language of the App
+  // });
+
+  // taken from:
+  // https://github.com/locize/react-tutorial/blob/main/step_2/src/i18n.js
+  i18n
+    // i18next-locize-backend
+    // loads translations from your project, saves new keys to it (saveMissing: true)
+    // https://github.com/locize/i18next-locize-backend
+    .use(LocizeBackend)
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+      debug: true,
+      lng: 'en',
+      fallbackLng: 'en',
+      saveMissing: true,
+      // keySeparator: false,
+
+      interpolation: {
+        escapeValue: false, // not needed for react as it escapes by default
+      },
+      backend: {
+        projectId: LOCIZE_PROJECT_ID,
+        apiKey: LOCIZE_API_KEY,
+        referenceLng: 'en',
+      },
+    });
 };
 
 initI18n();
--- apps/asdf/vite.config.ts
+++ apps/asdf/vite.config.ts
@@ -19,6 +19,8 @@ export default defineConfig({
 
   plugins: [react(), nxViteTsPaths()],
 
+  define: { 'process.env': process.env },
+
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
